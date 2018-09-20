00fc6488f974cc409ab9316f85a922a1f746eda6
Dale Barrie <dalebarrieit@gmail.com>
2018-09-20 22:06:13 +0100

    Calculating High/Low percentage and removing some unneded data fields

--- USD conversion rate.py
+++ USD conversion rate.py
@@ -3,4 +3,11 @@ import pandas as pd
 #this gets the historical data into pandas df
 df = pd.read_csv('GBP_USD_Historical_Data.csv')
 
-print(df.head)
+#this calculates the high low percentage
+df['HL_PCT'] = (df['High'] - df['Low']) / df['Low'] * 100.0
+
+
+# this again modifies the datafield with our own fields
+df = df[['Price','Open','HL_PCT', 'Change %']]
+
+print(df.head())
